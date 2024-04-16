ec8dab4c6fb8370aa6d00db3fa8f4953c5b0a8d6
Amish000 <amishchaudhary10@gmail.com>
2024-04-16 16:48:19 +0545

    added static templates and snippets

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='blog-home'),
-    path('about/', views.about, name='blog_about'),
+    path('about/', views.about, name='blog-about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Amish',
+        'title': 'Blog post 1',
+        'content': 'My first post',
+        'date_posted': 'April 16, 2024'
+    },
+    {
+        'author': 'CoreyMs',
+        'title': 'Blog post 2',
+        'content': 'My first post',
+        'date_posted': 'April 24, 2024'
+    },
+]
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog about<h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
