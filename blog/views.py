887151331dc40fe8520d9681e8bf4f0e3170f869
Amish000 <amishchaudhary10@gmail.com>
2024-04-23 13:58:56 +0545

    Used database in views.py

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author': 'Amish',
-        'title': 'Blog post 1',
-        'content': 'My first post',
-        'date_posted': 'April 16, 2024'
-    },
-    {
-        'author': 'CoreyMs',
-        'title': 'Blog post 2',
-        'content': 'My first post',
-        'date_posted': 'April 24, 2024'
-    },
-]
 # Create your views here.
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
